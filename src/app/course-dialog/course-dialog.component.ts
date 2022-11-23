fc4fb7293a768eafcc6d5663b9c5856233d94a2b
Veljko Vulovic <veljko.vulovic1@gmail.com>
2022-11-23 13:36:27 +0100

    Local Error Handling in an Angular Material Dialog

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -7,12 +7,13 @@ import {catchError} from 'rxjs/operators';
 import {throwError} from 'rxjs';
 import {CoursesService} from '../services/courses.service';
 import {LoadingService} from '../loading/loading.service';
+import {MessagesService} from '../messages/messages.service';
 
 @Component({
     selector: 'course-dialog',
     templateUrl: './course-dialog.component.html',
     styleUrls: ['./course-dialog.component.css'],
-    providers: [LoadingService]
+    providers: [LoadingService, MessagesService]
 })
 export class CourseDialogComponent implements AfterViewInit {
 
@@ -25,7 +26,8 @@ export class CourseDialogComponent implements AfterViewInit {
         private dialogRef: MatDialogRef<CourseDialogComponent>,
         @Inject(MAT_DIALOG_DATA) course:Course,
         private coursesService: CoursesService,
-        private loadingService: LoadingService) {
+        private loadingService: LoadingService,
+        private messagesService: MessagesService) {
 
         this.course = course;
 
@@ -46,14 +48,20 @@ export class CourseDialogComponent implements AfterViewInit {
 
       const changes = this.form.value;
 
-      const saveCourses$ = this.coursesService.saveCourse(this.course.id, changes);
+      const saveCourses$ = this.coursesService.saveCourse(this.course.id, changes).pipe(
+        catchError(err => {
+          const message = "Could not save course";
+          console.log(message, err);
+          this.messagesService.showErrors(message);
+          return throwError(err);
+        })
+      );
 
-
-        this.loadingService.showLoaderUntilCompleted(saveCourses$).subscribe(
-          val => {
-            this.dialogRef.close(val);
-          }
-        )
+      this.loadingService.showLoaderUntilCompleted(saveCourses$).subscribe(
+        val => {
+          this.dialogRef.close(val);
+        }
+      )
 
     }
 
