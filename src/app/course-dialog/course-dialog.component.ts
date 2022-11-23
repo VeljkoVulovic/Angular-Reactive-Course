19f882b98cab9dd8f50b1db768696ed92305c2d4
Veljko Vulovic <veljko.vulovic1@gmail.com>
2022-11-23 10:46:32 +0100

    Understanding the Angular Component providers property

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -11,7 +11,8 @@ import {LoadingService} from '../loading/loading.service';
 @Component({
     selector: 'course-dialog',
     templateUrl: './course-dialog.component.html',
-    styleUrls: ['./course-dialog.component.css']
+    styleUrls: ['./course-dialog.component.css'],
+    providers: [LoadingService]
 })
 export class CourseDialogComponent implements AfterViewInit {
 
@@ -45,8 +46,10 @@ export class CourseDialogComponent implements AfterViewInit {
 
       const changes = this.form.value;
 
-      this.coursesService.saveCourse(this.course.id, changes)
-        .subscribe(
+      const saveCourses$ = this.coursesService.saveCourse(this.course.id, changes);
+
+
+        this.loadingService.showLoaderUntilCompleted(saveCourses$).subscribe(
           val => {
             this.dialogRef.close(val);
           }
